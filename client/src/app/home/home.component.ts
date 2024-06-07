31cdedd1754783560eaf0075ff269c5f0f49d0f0
Mohamad-Dib <moha2004.md@gmail.com>
2024-06-07 20:27:29 +0300

    End of section 9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -14,10 +13,10 @@ users:any;
 registerMode=false;
 
   ngOnInit(): void {
-    this.getUsers();
-}
 
-constructor(private http:HttpClient) {
+  }
+
+constructor() {
     
 }
 
@@ -30,11 +29,5 @@ registerToggle(){
   this.registerMode = !this.registerMode;
 }
 
-getUsers(){
-  this.http.get("https://localhost:5001/api/users").subscribe({
-    next: response => this.users = response,
-    error: error => console.log(error),
-    complete: () => console.log("Request Completed")
-  });
-}
+
 }
